7fbf15d6266ef1a6058783a62359dc770e1d2b5c
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-11-08 22:57:25 +0600

    Hotel cost

---
+++ main.py
@@ -0,0 +1,18 @@
+hotelpay = float(input("Enter dollars spent on hotel : "))
+cab = float(input("Enter dollars spent on cab : "))
+flight = float(input("Enter dollars spent on flight : "))
+totalcost = hotelpay + cab + flight
+print("Total is ",totalcost)
+
+
+
+
+
+
+
+
+
+
+
+
+
